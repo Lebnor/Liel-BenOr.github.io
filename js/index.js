a16254cdb2f71277a81fc4c67d57d233563ebbe9
Liel <liel4349@example.com>
2021-12-22 16:41:27 +0200

    popsi colada and Network pages

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+});
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    });
+});
+
+window.onscroll = () => {
+    let a = document.head.scrollTop;
+    let b = document.head.scroll;
+    let c = document.head.scrollHeight;
+
+    if (a > 50) {
+        let h2 = document.querySelectorAll("h2");
+        h2.forEach((element) => (element.style.color = "red"));
+    }
+};
