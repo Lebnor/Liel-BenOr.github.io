e48507a4b012a8e7de6a3bcc529f6081e2392cc8
Liel <liel4349@example.com>
2022-01-29 22:18:08 +0200

    intro typewriter effect works on mobile the same as desktop

--- static/js/index.js
+++ static/js/index.js
@@ -15,51 +15,9 @@
 })();
 
 window.addEventListener("DOMContentLoaded", () => {
-  // intro typewriter effect for mobile
   let isMobile = window.matchMedia(
     "only screen and (max-width: 450px)"
   ).matches;
-
-  if (isMobile) {
-    // we want to cancel intro animations for mobile because they won't fit
-    let children = document.getElementsByClassName("typewriter-container")[0]
-      .children;
-    for (let i = 0; i < children.length; i++) {
-      children[i].innerText = "";
-    }
-
-    let head = "Hi, I am";
-    let sub = " Liel Ben-Or";
-
-    let headElem = document.querySelector("h1.typewriter.head");
-    let subElem = document.querySelector("h1.typewriter.sub");
-
-    // intro header
-    const headTimeout = setInterval(() => {
-      if (head) {
-        const char = head[0];
-        let curText = headElem.textContent.replace("|", "");
-        curText += char;
-        headElem.textContent = curText;
-        head = head.substring(1);
-      } else {
-        clearInterval(headTimeout);
-      }
-    }, 200);
-
-    // intro subtitle
-    const subStartAnimating = setTimeout(() => {
-      const writeToSub = setInterval(() => {
-        if (sub) {
-          const char = sub[0];
-          subElem.textContent = subElem.textContent + char;
-          sub = sub.substring(1);
-        } else {
-          clearInterval(writeToSub);
-        }
-      }, 200);
-    }, 2500);
-  }
 
   // "what i do" section fade-up effect when scrolled into view
   const servicesObserver = new IntersectionObserver(
@@ -90,13 +48,4 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // effects on page scroll
-window.onscroll = () => {
-  let a = document.head.scrollTop;
-  let b = document.head.scroll;
-  let c = document.head.scrollHeight;
-
-  if (a > 50) {
-    let h2 = document.querySelectorAll("h2");
-    h2.forEach((element) => (element.style.color = "red"));
-  }
-};
+window.onscroll = () => {};
